8dddf24d66d5086359e17acd1cf9aa0aa8e9768f
Cristian <47904104+cristianc81@users.noreply.github.com>
2026-05-27 18:01:07 +0200

    Update webhook routes to include 'webhook' prefix

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use MissaelAnda\Whatsapp\Http\Controllers\WebhookController;
 
-Route::get('', [WebhookController::class, 'subscribe'])->name('webhook.subscribe');
-Route::post('', [WebhookController::class, 'handle'])->name('webhook');
+Route::get('webhook', [WebhookController::class, 'subscribe'])->name('webhook.subscribe');
+Route::post('webhook', [WebhookController::class, 'handle'])->name('webhook');
